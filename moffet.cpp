7b90ae06612e1dc62e35c76f04e66931895865aa
WRenchJr42 <yashas140304@gmail.com>
2023-10-07 20:54:40 +0530

    update .cpp file

--- moffet.cpp
+++ moffet.cpp
@@ -1,6 +1,9 @@
 #include <string> 
 #include <iostream>
+#include <vector>
 using namespace std;
+
+/*-----------------LEXER---------------------*/
 
 enum Token  // List of tokens
 {
@@ -81,7 +84,7 @@ static int gettok() // Takes the token for processing.
         return ThisChar;
     
 }
-
+/*
 int main()
 {
     while(true)
@@ -90,3 +93,99 @@ int main()
             cout << "Lexer Got Token:" << tok << endl;
     }
 }
+*/
+
+/*----------------END OF LEXER-----------------*/
+
+/*-----------------PARSER------------------*/
+
+class ExpressAST
+{
+    public:
+        virtual ~ExpressAST()
+        {
+
+        }
+};
+
+class NumExpressAST : public ExpressAST
+{
+    double val;
+
+    public:
+        NumExpressAST(double Val) : val(Val)
+        {
+
+        }
+};
+
+class VarExpressAST : public ExpressAST
+{
+    std::string burp;
+
+    public:
+        VarExpressAST(const std::string & Burp) : burp(Burp)
+        {
+
+        }
+};
+
+class BinExpressAST : public ExpressAST
+{
+    char Op;
+    std::unique_ptr<ExpressAST> LHS, RHS;
+
+    public:
+        BinExpressAST(
+            char op,
+            std::unique_ptr<ExpressAST> LHS,
+            std::unique_ptr<ExpressAST> RHS
+        ): Op(op),LHS(std::move(LHS)),RHS(std::move(RHS))
+        {
+
+        }
+};
+
+class CallExpressAST : public ExpressAST
+{
+    std::string Callee;
+    std::vector<std::unique_ptr<ExpressAST>> Args;
+
+    public:
+        CallExpressAST(const std::string & Callee,
+        std::vector<std::unique_ptr<ExpressAST>> Args)
+        : Callee(Callee), Args(std::move(Args))
+        {
+
+        }
+};
+
+class PrototypeAST
+{
+    std::string Burp;
+    std::vector<std::string> Args;
+
+    public:
+        PrototypeAST(const std::string & Burp,
+        std::vector<std::string> Args)
+        :Burp(Burp), Args(std::move(Args))
+        {
+
+        }
+};
+
+class FunAST
+{
+    std:: unique_ptr<PrototypeAST> Pro;
+    std:: unique_ptr<ExpressAST> Body;
+
+    public:
+        FunAST(std:: unique_ptr<ProAST> Pro,
+        std:: unique_ptr<ExpressAST> Body)
+        :Pro(std::move(Pro)),Body(std::move(Body))
+        {
+
+        }
+};
+
+/*-----TO BE CONTINUED------- Dont try it now , its super buggy*/
